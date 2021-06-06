527fa8d542d66954c92f4e7b90ea7d8bd40cb1e2
Atilio <atiliogoulart@gmail.com>
2021-06-06 20:15:10 -0300

    Update 1038.py

--- 1038.py
+++ 1038.py
@@ -1,3 +1,27 @@
-code = int(input())
-quantity = int(input())
+values = [int(i) for i in input().split()]
 
+code = values[0]
+quantity = values[1]
+
+price = 0
+
+if code == 1:
+    price = 4.0
+    value = (price*quantity)
+    print('Total: R$ {:.2f}'.format(value))
+elif code ==2:
+    price = 4.5
+    value = (price*quantity)
+    print('Total: R$ {:.2f}'.format(value))
+elif code ==3:
+    price = 5.0
+    value = (price*quantity)
+    print('Total: R$ {:.2f}'.format(value))
+elif code ==4:
+    price = 2.0
+    value = (price*quantity)
+    print('Total: R$ {:.2f}'.format(value))
+elif code ==5:
+    price = 1.5
+    value = (price*quantity)
+    print('Total: R$ {:.2f}'.format(value))
